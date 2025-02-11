24815ad3b232e98b5e760b1f0e944c67d46ca0f6
CarlosEduardo-IFF <almeida.carlos@gsuite.iff.edu.br>
2025-02-11 18:21:33 -0300

    first commit

---
+++ src/test/java/edu/iff/project/sustainableconnection/SustainableconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iff.project.sustainableconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainableconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
