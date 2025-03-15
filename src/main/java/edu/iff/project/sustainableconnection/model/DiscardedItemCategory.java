05959c91708f95beecb497a080fa3b85061cfdf9
Ruan Manhães <91421063+Ruan0Daniel@users.noreply.github.com>
2025-03-15 10:48:46 -0300

    Manter categorias para itens descartados

--- src/main/java/edu/iff/project/sustainableconnection/model/DiscardedItemCategory.java
+++ src/main/java/edu/iff/project/sustainableconnection/model/DiscardedItemCategory.java
@@ -1,5 +1,50 @@
 package edu.iff.project.sustainableconnection.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "discarded_item_category")
 public class DiscardedItemCategory {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	private String name;
+	private int pointsPerItens;
+	
+    public DiscardedItemCategory() {}
+
+    public DiscardedItemCategory(String name, int pointsPerItens) {
+        this.setName(name);
+        this.setPointsPerItens(pointsPerItens);
+    }
+
+	public int getPointsPerItens() {
+		return pointsPerItens;
+	}
+
+	public void setPointsPerItens(int pointsPerItens) {
+		this.pointsPerItens = pointsPerItens;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
--- src/main/java/edu/iff/project/sustainableconnection/repository/DiscardedItemCategoryRepository.java
+++ src/main/java/edu/iff/project/sustainableconnection/repository/DiscardedItemCategoryRepository.java
@@ -1,5 +1,9 @@
 package edu.iff.project.sustainableconnection.repository;
 
-public class DiscardedItemCategoryRepository {
-    
+import edu.iff.project.sustainableconnection.model.DiscardedItemCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscardedItemCategoryRepository extends JpaRepository<DiscardedItemCategory, Long> {
 }
